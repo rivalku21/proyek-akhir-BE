797f584166faebb0ab2c81ef3a0b99ffdb965e4e
Rival Fauzi <rival4267@gmail.com>
2021-08-10 22:13:39 +0700

    update

--- src/api/api.js
+++ src/api/api.js
@@ -3,35 +3,9 @@ const fs = require('fs');
 const pdfParser = require('pdf2json')
 
 controller_object.contoh_fungsi = async(req, res, next) => {
-    string1="string1";
-    string2="string2 is here";
-        changepercent(string1,string2);
-    }
-    
-    function changepercent(string1,string2) {
-    
-        var s1Parts= string1.split(' ');
-        var s2Parts= string2.split(' ');
-    
-        var matched = 0;
-    
-    for(var i = 0; i<s1Parts.length; i++)
-    {
-        for(var j = 0; j<s2Parts.length; j++)
-            {
-            if(s1Parts[i] === s2Parts[j])
-                matched++;   
-            }
-    
-    }
-        var percentage=(matched/Math.max(s1Parts.length, s2Parts.length))*100;
-    
-        console.log(matched);
-        console.log(percentage);
-    if(percentage<50)
-    {
-        console.log("Change Above 50%");
-    }
+    res.status(200).json({
+        status: "sukses"
+    });
 };
 
 module.exports = controller_object;
